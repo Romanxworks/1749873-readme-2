998766d6ddb5138ad3cbada8374624db2d82338b
roman-chv <chv-roman@mail.ru>
2022-12-14 18:45:59 +0300

    users: добавлена валидация переменных окружения

--- readme/apps/users/src/app/app.module.ts
+++ readme/apps/users/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './auth/auth.module';
 import { ENV_FILE_PATH } from './app.constant';
 import { ConfigModule } from '@nestjs/config';
 import databaseConfig from '../config/database.config';
+import envSchema from './env.schema';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import databaseConfig from '../config/database.config';
       cache: true,
       isGlobal: true,
       envFilePath: ENV_FILE_PATH,
-      load: [databaseConfig]
+      load: [databaseConfig],
+      validationSchema: envSchema
     }),
     UserModule,
     AuthModule],
